422200df9ca2dada6929bb413a5739ed2c58902e
sspathare97 <sspathare97@gmail.com>
2021-11-05 20:20:53 -0600

    ejs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
-const db = require('./services/db');
+const db = require('./db');
 
 const app = express();
 const port = process.env.PORT;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', async (req, res) => {
-  try {
-    const rows = await db.query('SELECT 1');
-    console.log({rows});
-  }
-  catch (error) {
-    console.log(error);
-  }
-  res.send('Hello World!');
+  res.render('index', {
+    data: 'Hello World!'
+  });
 })
 
 app.listen(port, () => {
