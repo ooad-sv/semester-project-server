b66ba95d20e84cd60a6ce977976c9b8c9345f5f1
sspathare97 <sspathare97@gmail.com>
2021-11-05 19:39:35 -0600

    connection urli

--- src/config.js
+++ src/config.js
@@ -2,11 +2,7 @@ const env = process.env;
 
 const config = {
   db: {
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+    connectionString: env.DATABASE_URL,
   },
 };
 
