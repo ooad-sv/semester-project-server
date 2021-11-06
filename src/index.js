c8ab815c6a21580d81a351efd36f975dc25c8d77
sspathare97 <sspathare97@gmail.com>
2021-11-05 19:24:58 -0600

    db connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./services/db');
+
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const rows = await db.query('SELECT 1');
+  console.log({rows});
   res.send('Hello World!');
 })
 
