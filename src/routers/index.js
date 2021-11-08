92bbed3287a0d9270c537bc7ff6fb9cad1146e3f
sspathare97 <sspathare97@gmail.com>
2021-11-07 20:05:14 -0700

    validation

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -14,13 +15,22 @@ router.get('/register', async (req, res) => {
     res.render('register', {title: 'Register'});
 });
 
-router.post('/register', async (req, res) => {
-    console.log(req.body);
-    const { email, password, confirmPassword } = req.body;
-    if (password !== confirmPassword) {
-        res.render('register', {title: 'Register', errors: ["Passwords don't match!"]});
+router.post('/register', 
+    body('email', 'Email not valid!').trim().isEmail().normalizeEmail(),
+    body('password', 'Password must be at least 4 characters long!').trim().isLength({ min: 4 }),
+    body('confirmPassword', 'Passwords must be the same!').custom(async (confirmPassword, {req}) => {
+        if (req.body.password !== confirmPassword) throw new Error();
+    }),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.array());
+            return res.render('register', {title: 'Register', errors: errors.array().map(e => e.msg)});
+        }
+        console.log(req.body);
+        const { email, password } = req.body;
+        res.render('register', {title: 'Register'});
     }
-    res.render('register', {title: 'Register'});
-});
+);
 
 module.exports = router;
